2c8d1ca807b3fdfe9e14dc8d138e35b9f0a459d4
payamaya <paulyashou81@gmail.com>
2025-05-07 18:05:11 +0200

    fix: Update API_BASE_URL and login API_URL to point to the correct production server

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,2 +1,2 @@
-const API_BASE_URL = 'https://vite-apl-ef1z.vercel.app/api/'
+const API_BASE_URL = 'https://vite-apl.vercel.app/api/'
 export default API_BASE_URL
--- src/api/login.ts
+++ src/api/login.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const API_URL = 'https://reqres.in/api/login'; // Fake API
+const API_URL = 'https://vite-apl-ef1z.vercel.app/' // Fake API
 
 const login = async (credentials: { email: string; password: string }) => {
-  const response = await axios.post(`${API_URL}/login`, credentials);
-  return response.data.token;
-};
+  const response = await axios.post(`${API_URL}/login`, credentials)
+  return response.data.token
+}
 
-export default { login };
+export default { login }
