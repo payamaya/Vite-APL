1177e2e9f490161c55069e2a75a031ecb92093d6
payamaya <paulyashou81@gmail.com>
2025-05-07 18:10:45 +0200

    fix: Clean up comments and import statements in GoBackButton component

--- src/Components/common/buttons/GoBackButton.tsx
+++ src/Components/common/buttons/GoBackButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom' // Use useNavigate from react-router-dom v6
+import { useNavigate } from 'react-router-dom'
 import ReusableButton from './ReusableButton'
-// Assuming ReusableButton is in this location
 
 // GoBackButton component
 const GoBackButton: React.FC = () => {
@@ -14,10 +13,10 @@ const GoBackButton: React.FC = () => {
   return (
     <ReusableButton
       as='button'
-      onClick={handleGoBack} // Trigger the handleGoBack function on button click
+      onClick={handleGoBack}
       label='Go Back'
       // startIcon={<FaArrowLeft />} Add Icon If
-      theme='light' // You can modify this based on your theme requirements
+      theme='light'
     />
   )
 }
