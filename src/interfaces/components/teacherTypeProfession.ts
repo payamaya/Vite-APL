553c4dcb51c23e79fbc04b593fe53447c4b26177
payamaya <paulyashou81@gmail.com>
2025-05-08 20:28:34 +0200

    refactor: Remove unused teacherTypeProfession interface file for cleaner code

--- src/interfaces/components/teacherTypeProfession.ts
+++
@@ -1,8 +0,0 @@
-// import { TeacherTypeProfession } from './ITeacher';
-
-// export const teacherTypeProfession: { label: string; value: TeacherTypeProfession }[] = [
-//   { value: 'it', label: 'IT' },
-//   { value: 'matematik', label: 'Matematik' },
-//   { value: 'design', label: 'Design' },
-//   { value: 'teknik', label: 'Teknik' },
-// ]
