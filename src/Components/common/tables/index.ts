e407c96c2446785e02f8cefb3f9dce0f2f94c03d
payamaya <paulyashou81@gmail.com>
2025-06-15 17:03:27 +0200

    Add studentTableColumns and update imports in AdminCourses

---
+++ src/Components/common/tables/index.ts
@@ -0,0 +1,4 @@
+export { default as studenTableColumns } from './studentTableCoulmns'
+export { default as noticeTableColumns } from './noticeTableColumns'
+export { default as courseTableColumns } from './courseTableColumns'
+export { default as ReusableTable } from './ReusableTable'
